3e72548d48821c0b9c03dce1a67b44f80fbee222
Jeffrey Simon <jdsimon@gmail.com>
2020-09-20 22:35:56 -0400

    Clean up and notes

--- src/hpa_calc/core.cljs
+++ src/hpa_calc/core.cljs
@@ -33,28 +33,36 @@
                                       (-> (assoc app-state property new-value)
                                           calc-desired-replicas)))))}]]))
 
+(defn replicas-color-phrase [current-replicas desired-replicas]
+  (cond
+    (= current-replicas desired-replicas)
+    ["green" "Optimal"]
+
+    (< current-replicas desired-replicas)
+    ["red" "Underprovisioned"]
+
+    :else
+    ["orange" "Overprovisioned"]))
+
 (defn container []
   [:section {:class "mw5 mw7-ns center bg-light-gray pa3 ph5-ns"}
    [:h3 {:class "tc"}
     (let [current-replicas (:current-replicas @variables)
           desired-replicas (:desired-replicas @variables)
-          [color phrase]
-          (cond
-            (= current-replicas desired-replicas)
-            ["green" "Optimal"]
-
-            (< current-replicas desired-replicas)
-            ["red" "Underprovisioned"]
-
-            :else
-            ["orange" "Overprovisioned"])]
+          [color phrase] (replicas-color-phrase current-replicas desired-replicas)]
       [:span {:class color} phrase])]
    [:h3 {:class "tc"} (str "Desired Replicas: " (:desired-replicas @variables))]
    [:h3 {:class "tc"} (str "Current Average Request Count: " (:current-average-metric @variables))]
-   #_[slider "Desired Replicas" :desired-replicas 1 20]
    [slider "Current Replicas" :current-replicas 1 20]
-   [slider "Current Request Count" :current-metric 0 1000]
-   [slider "Desired Average Request Count" :desired-average-metric 0 1000]])
+   [slider "Current Request Count" :current-metric 0 300]
+   [slider "Desired Average Request Count" :desired-average-metric 0 100]
+   [:div {:class "ph0 courier f6 pa3"}
+    [:h4 {:class "pa0 f6 ph0"} "Notes"]
+    [:ul {:class "ph3"}
+     [:li {:class "ph0 pa2"} "The above is based on the " [:a {:target "_blank" :href "https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale/#algorithm-details"} "HPA formula"] "."]
+     [:li {:class "ph0 pa2"} "Current Average Request Count = ceil(Current Request Count / Current Replicas)"]
+     [:li {:class "ph0 pa2"} "Desired Average Request Count is the target average value, the amount of traffic each replica will handle."]
+     [:li {:class "ph0 pa2"} "Current Request Count is the raw metric value pulled from Datadog."]]]])
 
 (defn mount [el]
   (rdom/render
